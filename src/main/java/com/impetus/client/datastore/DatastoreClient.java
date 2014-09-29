e25f04783acb912980176b65a730d2ac2183c0cd
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2014-09-29 15:49:21 +0200

    modified the way ids are handled

--- src/main/java/com/impetus/client/datastore/DatastoreClient.java
+++ src/main/java/com/impetus/client/datastore/DatastoreClient.java
@@ -7,38 +7,39 @@ import com.impetus.kundera.PersistenceProperties;
 import com.impetus.kundera.client.Client;
 import com.impetus.kundera.client.ClientBase;
 import com.impetus.kundera.db.RelationHolder;
+import com.impetus.kundera.generator.AutoGenerator;
 import com.impetus.kundera.index.IndexManager;
 import com.impetus.kundera.metadata.KunderaMetadataManager;
 import com.impetus.kundera.metadata.model.ClientMetadata;
 import com.impetus.kundera.metadata.model.EntityMetadata;
 import com.impetus.kundera.metadata.model.MetamodelImpl;
 import com.impetus.kundera.metadata.model.PersistenceUnitMetadata;
+import com.impetus.kundera.metadata.model.attributes.AbstractAttribute;
+import com.impetus.kundera.metadata.model.type.AbstractManagedType;
 import com.impetus.kundera.persistence.EntityManagerFactoryImpl.KunderaMetadata;
 import com.impetus.kundera.persistence.EntityReader;
 import com.impetus.kundera.persistence.context.jointable.JoinTableData;
 import com.impetus.kundera.property.PropertyAccessorHelper;
 import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.metamodel.Attribute;
 import javax.persistence.metamodel.EntityType;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Fabio Arcidiacono.
  *         <p>The gateway to CRUD operations on database, except for queries.</p>
  */
-public class DatastoreClient extends ClientBase implements Client<DatastoreQuery> {
-    // for auto id generation implement AutoGenerator
+public class DatastoreClient extends ClientBase implements Client<DatastoreQuery>, AutoGenerator {
 
     private static final Logger logger = LoggerFactory.getLogger(DatastoreClient.class);
     private EntityReader reader;
     private DatastoreService datastore;
+    private Random random;
     private int batchSize;
 
     protected DatastoreClient(final KunderaMetadata kunderaMetadata, Map<String, Object> properties,
@@ -49,6 +50,7 @@ public class DatastoreClient extends ClientBase implements Client<DatastoreQuery
         this.datastore = datastore;
         this.indexManager = indexManager;
         this.clientMetadata = clientMetadata;
+        this.random = new Random();
         setBatchSize(persistenceUnit, this.externalProperties);
     }
 
@@ -87,49 +89,78 @@ public class DatastoreClient extends ClientBase implements Client<DatastoreQuery
 
         EntityType entityType = metamodel.entity(entityMetadata.getEntityClazz());
 
-        Entity gaeEntity = new Entity(entityType.getName());
-        handleAttributes(gaeEntity, entity, entityType.getAttributes());
+        // TODO decide
+        // this way datastore generate id
+        // entity must have an id annotated with @Id
+        // and must have a value different from null
+        // Entity gaeEntity = new Entity(entityType.getName());
+
+        // is also possible to use the one generated
+        // need to define a Long id annotated with @Id
+        // and with @GeneratedValue()
+        // apparently is also setted back to the entity
+        Entity gaeEntity = new Entity(entityType.getName(), (Long) id);
+
+        System.out.println(entityMetadata.getIdAttribute());
+
+        handleAttributes(gaeEntity, entity, metamodel, entityMetadata, entityType.getAttributes());
         handleRelations(gaeEntity, rlHolders);
         handleDiscriminatorColumn(gaeEntity, entityType);
 
         datastore.put(gaeEntity);
     }
 
-    private void handleAttributes(Entity gaeEntity, Object entity, Set<Attribute> attributes) {
+    @Override
+    public Object generate() {
+        return random.nextLong();
+    }
+
+    private void handleAttributes(Entity gaeEntity, Object entity, MetamodelImpl metamodel, EntityMetadata metadata, Set<Attribute> attributes) {
         for (Attribute attribute : attributes) {
-            // by pass association. TODO understand this isAssociation()
-            if (!attribute.isAssociation()) {
-                Field field = (Field) attribute.getJavaMember();
-                Object value = PropertyAccessorHelper.getObject(entity, field);
-                gaeEntity.setProperty(attribute.getName(), value);
+            // by pass id attribute, use datastore one
+            if (!attribute.equals(metadata.getIdAttribute())) {
+                // by pass association.   TODO verify this
+                if (!attribute.isAssociation()) {
+                    if (metamodel.isEmbeddable(((AbstractAttribute) attribute).getBindableJavaType())) {
+                        processEmbeddableAttribute(entity, metamodel, metadata, attribute);
+                    } else {
+                        Field field = (Field) attribute.getJavaMember();
+                        Object valueObj = PropertyAccessorHelper.getObject(entity, field);
+
+                        if (valueObj != null) {
+                            gaeEntity.setProperty(attribute.getName(), valueObj);
+                        }
+                    }
+                }
             }
         }
     }
 
+    private void processEmbeddableAttribute(Object entity, MetamodelImpl metamodel, EntityMetadata metadata, Attribute attribute) {
+        // TODO Auto-generated method stub
+    }
+
     private void handleRelations(Entity gaeEntity, List<RelationHolder> rlHolders) {
-        //System.out.println("gaeEntity = [" + gaeEntity + "], rlHolders = [" + rlHolders + "]");
-        // TODO Iterate over relations
-        // if (rlHolders != null && !rlHolders.isEmpty()) {
-        //     for (RelationHolder rh : rlHolders) {
-        //         String relationName = rh.getRelationName();
-        //         Object valueObj = rh.getRelationValue();
-        //
-        //         if (!StringUtils.isEmpty(relationName) && valueObj != null) {
-        //
-        //         }
-        //     }
-        // }
+        if (rlHolders != null && !rlHolders.isEmpty()) {
+            for (RelationHolder rh : rlHolders) {
+                String relationName = rh.getRelationName();
+                // TODO maybe need a key or something like parent, not valueObj
+                Object valueObj = rh.getRelationValue();
+
+                if (!StringUtils.isEmpty(relationName) && valueObj != null) {
+                    gaeEntity.setProperty(relationName, valueObj);
+                }
+            }
+        }
     }
 
     private void handleDiscriminatorColumn(Entity gaeEntity, EntityType entityType) {
-        //System.out.println("gaeEntity = [" + gaeEntity + "], entityType = [" + entityType + "]");
-        // TODO discriminator
-        // String discrColumn = ((AbstractManagedType) entityType).getDiscriminatorColumn();
-        // String discrValue = ((AbstractManagedType) entityType).getDiscriminatorValue();
-        //
-        // if (discrColumn != null && discrValue != null) {
-        //
-        // }
+        String discrColumn = ((AbstractManagedType) entityType).getDiscriminatorColumn();
+        String discrValue = ((AbstractManagedType) entityType).getDiscriminatorValue();
+
+        if (discrColumn != null && discrValue != null) {
+            gaeEntity.setProperty(discrColumn, discrValue);
+        }
     }
 
     @Override
