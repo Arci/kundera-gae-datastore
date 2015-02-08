9c3e7cb10f9635056ca39f3edd60276115032e80
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2015-02-08 18:01:46 +0100

    updated persistence.xml

--- src/main/java/it/polimi/kundera/client/datastore/config/DatastoreConstants.java
+++ src/main/java/it/polimi/kundera/client/datastore/config/DatastoreConstants.java
@@ -9,8 +9,6 @@ public class DatastoreConstants {
 
     public static final int DEFAULT_PORT = 443;
 
-    public static final String ROOT_NAME = "datastore.fictitious";
-
     public static final String READ_POLICY = "datastore.policy.read";
     public static final String TRANSACTION_POLICY = "datastore.policy.transaction";
     public static final String DEADLINE = "datastore.deadline";
